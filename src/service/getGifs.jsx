82d71e66d3fdad5252dde65d614b531f9b48967e
Juamez <juanmendezs.a@gmail.com>
2022-05-14 19:30:58 -0500

    add apikey, settings

--- src/service/getGifs.jsx
+++ src/service/getGifs.jsx
@@ -1,20 +1,22 @@
-const apiKey = 'eznCSt5uJaLzyX7yPjwiy6IQ5MpW0XBd' 
+import { API_KEY, API_URL } from './settings'
+
+const fromApiResponseToGifs = apiResponse => {
+  const {data = []} = apiResponse
+  if(Array.isArray(data)) {
+    const gifs = data.map(image => {
+      const { url } = images.downsized_medium
+      return { title, id, url }
+    })
+    return gifs
+  }
+  return []
+}
 
 
-export default function getGifs({keyword = '404'} = {}) {
-    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
+export default function getGifs({limit = 25, keyword = '404', page = 0} = {}) {
+    const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=G&lang=en`
     
     return fetch(apiURL)
       .then(res => res.json())
-      .then(response => {
-        const {data = []} = response
-        if(Array.isArray(data)) {
-            const gifs = data.map(image => {
-                const {images, title, id} = image
-                const {url} = images.downsized_medium
-                return {title, id, url} 
-            })
-            return gifs
-        }
-      })
+      .then(fromApiResponseToGifs)
 }
---
+++ src/service/settings.jsx
@@ -0,0 +1,3 @@
+export const API_KEY = 'eznCSt5uJaLzyX7yPjwiy6IQ5MpW0XBd'
+
+export const API_URL = 'https://api.giphy.com/v1' 
