9a0551fecb1f08f5fca39706c49f23ca502b9379
Juamez <juanmendezs.a@gmail.com>
2022-05-14 19:29:33 -0500

    add each component

--- src/components/ListOfGifs/listOfGifs.jsx
+++ src/components/ListOfGifs/listOfGifs.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import Gif from "../Gif";
+import Gif from "../Gif"
+import './style.css'
 
-export default function ListOfGifs( {gifs} ) {
+export default function ListOfGifs({ gifs }) {
     return <div className='ListOfGifs'>
         {
             gifs.map(({ id, title, url }) => 
--- src/components/spinner/spinner.jsx
+++ src/components/spinner/spinner.jsx
@@ -1,16 +1,6 @@
-import './style.css'
+import React from 'react'
+import './styles.css'
 
-export default function Spinner() {
-  return (
-    <div id="floatingCirclesG">
-	    <div className="f_circleG"></div>
-	    <div className="f_circleG"></div>
-	    <div className="f_circleG"></div>
-	    <div className="f_circleG"></div>
-	    <div className="f_circleG"></div>
-	    <div className="f_circleG"></div>
-	    <div className="f_circleG"></div>
-	    <div className="f_circleG"></div>
-    </div>
-  )
+export default function Spinner () {
+  return <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
 }
